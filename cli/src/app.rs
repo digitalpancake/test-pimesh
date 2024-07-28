982aea3acd4589210e056affd0e50e6b383154ad
Jules White <juleswhite@127.0.0.1>
2024-07-28 18:51:31 +0100

    Display selected index in block, add select next and prev controls

--- cli/src/app.rs
+++ cli/src/app.rs
@@ -1,9 +1,23 @@
 use std::io;
 
 use crate::tui;
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Alignment, Rect},
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{
+        block::{Position, Title},
+        Block, Paragraph, Widget,
+    },
+    Frame,
+};
 
 #[derive(Debug, Default)]
 pub struct App {
+    selected: u8,
     exit: bool,
 }
 
@@ -16,9 +30,72 @@ impl App {
         Ok(())
     }
 
-    fn render_frame(&self, frame: &mut Frame) {}
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
 
     fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
         Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Up => self.select_prev(),
+            KeyCode::Down => self.select_next(),
+            KeyCode::Char('q') => self.exit(),
+            _ => {},
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn select_next(&mut self) {
+        self.selected += 1
+    }
+    fn select_prev(&mut self) {
+        self.selected -= 1
+    }
+
+
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from("PiMesh CLI".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Next".into(),
+            "<Down>".green().bold(),
+            " Previous".into(),
+            "<Up>".green().bold(),
+            " Quit".into(),
+            "<Q>".green().bold(),
+        ]));
+
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::ROUNDED);
+
+        let selected_text = Text::from(vec![Line::from(vec![
+                                                       "Value:".into(),
+                                                       self.selected.to_string().blue(),
+        ])]);
+
+        Paragraph::new(selected_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
--- cli/src/tui.rs
+++ cli/src/tui.rs
@@ -11,37 +11,19 @@ use ratatui::{
 };
 use std::io::{stdout, Result, Stdout};
 
+use crate::app::App;
+
 pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 
 pub fn run_tui() -> Result<()> {
 
     let mut terminal = tui_init()?;
 
-    loop {
-        terminal.draw(|frame| {
-            let area = frame.size();
-            frame.render_widget(
-                Paragraph::new("PiMesh CLI ( 'q' to quit )")
-                    .white()
-                    .on_green(),
-                area,
-            );
-        })?;
-
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    break;
-                }
-            }
-        }
-    }
-
-    // Cleanup
+    let res = App::default().run(&mut terminal);
 
     stdout().execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
-    Ok(())
+    res
 }
 
 fn tui_init() -> Result<Tui> {
